c14850d953c7b4fcceee2ef6570493d9e0a7c8da
miloszfoksinski <53444641+miloszfoksinski@users.noreply.github.com>
2019-08-29 20:48:40 +0200

    Update ex_3.py

--- ex_3.py
+++ ex_3.py
@@ -1,1 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
 
+result = requests.get('https://wiedzmin.pl/wiedzmin-ksiazka/wiedzmin-cytaty/ostatnie-zyczenie-cytaty')
+src = result.content  
+soup = BeautifulSoup(src, 'lxml')
+quotes = soup.find_all('ul')
