3426aa882acf1564b92ad2770725d2ca4a31a036
miloszfoksinski <53444641+miloszfoksinski@users.noreply.github.com>
2019-08-29 20:41:16 +0200

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,1 +1,20 @@
+import requests
+from bs4 import Beautifulsoup
+
+result = requests.get("https://www.google.pl/")
+
+#print(result.status_code) Sprawdzenie czy strona jest dostępna
+#print(result.headers) Sprawdzenie nagłówków HTTP
+
+src = result.content #przypisanie zawartości strony do zmiennej
+#print(src)
+
+soup = Beautifulsoup(src,'lxml') #Tworzymy obiekt Beautifulsoup oparty na zmiennej źródłowej src
+links = soup.find_all('a')#Szukamy akpitów <a>, rezultatem będzie lista
+print(links)
+
+for link in links:
+  if "About" in link.text :
+    print(link)
+    print(link.attrs['href'])
 
