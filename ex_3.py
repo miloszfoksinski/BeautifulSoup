f74e7805668c8aacceb1f9be20be0ffbcde81143
miloszfoksinski <miloszfoksinski@gmail.com>
2019-08-31 22:21:14 +0200

    try to extract all names and temperatures

--- ex_3.py
+++ ex_3.py
@@ -9,7 +9,11 @@ soup = BeautifulSoup(page.content, 'html.parser')
 cities_blocks = soup.find(class_='weather-index')
 cities = cities_blocks.find_all('a')
 
-print(cities[0].string.strip())
+# print(cities[0].string.strip())
+temperatures = cities_blocks.find_all('span')
+print(temperatures[0].string)
+
+citiesnames = [ city.find('a').get_text() for city in cities]
 #city = cities.find('a')
 #print(city.text)
 
