e3a2408505ee5172d92b9ada30c53b9a732adfd2
miloszfoksinski <miloszfoksinski@gmail.com>
2019-08-31 11:25:18 +0200

    receive quotes

--- ex_3.py
+++ ex_3.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+f = csv.writer(open('Witcher_Quotes', 'w'))
+
 
 result = requests.get('https://wiedzmin.pl/wiedzmin-ksiazka/wiedzmin-cytaty/ostatnie-zyczenie-cytaty')
 src = result.content  
 soup = BeautifulSoup(src, 'lxml')
-quotes = soup.find_all('ul')
+quotes = []
+for ul_tag in soup.find_all('ul') :
+    li = ul_tag.find('li')
+    quotes.append(li.text)
+print(quotes[8:])
