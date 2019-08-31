f98541e6551932045cfa7d9f3b321ae457f2b9be
miloszfoksinski <miloszfoksinski@gmail.com>
2019-08-31 14:23:54 +0200

    extract cities

--- ex_3.py
+++ ex_3.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+# import pandas
+
+page = requests.get('https://pogoda.interia.pl/')
+soup = BeautifulSoup(page.content, 'html.parser')
+# print(soup)
+cities = soup.find(class_='weather-index')
+# print(cities)
 
